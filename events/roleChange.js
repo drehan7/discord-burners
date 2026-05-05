c437e3d535cacfb4eaabb40da8cf288bacb35b68
Dawud Rehan <d.rehan7@gmail.com>
2026-05-04 23:36:41 -0400

    send message to channel if new burner

--- events/roleChange.js
+++ events/roleChange.js
@@ -1,17 +1,40 @@
 const { Events } = require('discord.js');
+const {welcomeChannel} = require('../config.json');
+
+function isNewBurner(oldMember, newMember) {
+    // Has Burner role in new, not in old
+    const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+
+    const found = addedRoles.find(r => r.name === "test");
+
+    return !!found;
+}
 
 module.exports = {
     name: Events.GuildMemberUpdate,
-    execute(oldMember, newMember) {
-        console.log('Member updated!');
+    async execute(oldMember, newMember) {
 
-        console.log('old:', oldMember.roles.cache);
+        // If update to member is role change to burner
+        if (!isNewBurner(oldMember, newMember)) return;
 
-        const member = oldMember.user.username;
-        const old = oldMember.roles.cache.role[1].name;
-        const newRole = newMember.roles.cache.role[1].name;
+        // Get Member object
+        const member = oldMember.user;
 
-        // Make sure member went from 'tempy' to 'Burner'
-        console.log(`Member: ${memberName} role from ${old} to ${newRole}`);
+        // Pings member
+        const welcomeMessage = `Welcome ${member}! Please get comfortable, read through the handbook, and choose whatever roles you like. We are glad you found us. How are you doing today?`;
+
+        // Get reference to client
+        const client = oldMember.client;
+        if (!welcomeChannel) {
+            console.error('welcomeChannel not found');
+            return;
+        }
+
+        // Get channel to send meesage to
+        const channel = client.channels.cache.get(welcomeChannel);
+        channel.sendTyping(); // 'bot' is typing...
+        if (channel) {
+            await channel.send(welcomeMessage);
+        }
     },
 };
